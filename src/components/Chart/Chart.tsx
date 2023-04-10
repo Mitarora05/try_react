d5d479e09aaf2bc66611a93da76312156a46f36c
mitarora <mitarora@deloitte.com>
2023-04-10 19:44:18 +0530

    adding Chart

---
+++ src/components/Chart/Chart.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Chart.css";
+import ChartBar from "./ChartBar";
+
+type DataPoint = {
+  key: any;
+  value: any;
+  maxValue: number | null;
+  label: any;
+};
+
+type ChartProps = {
+  dataPoints: DataPoint[];
+};
+
+const Chart: React.FC<ChartProps> = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint: DataPoint) => (
+        <ChartBar 
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
---
+++ src/components/Chart/ChartBar.tsx
@@ -0,0 +1,5 @@
+const ChartBar = () => {
+    return <div>bar</div>
+}
+
+export default ChartBar;
