da0c9805e8d6554df7f39083b826528102d2b989
mitarora <mitarora@deloitte.com>
2023-04-10 14:29:41 +0530

    useState

--- src/components/ExpenseItem/ExpenseItem.tsx
+++ src/components/ExpenseItem/ExpenseItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import "./ExpenseItem.css";
 
@@ -9,18 +10,20 @@ export default function ExpenseItemComponent(props: any) {
   //   const expenseDate = new Date(2023, 4, 7);
   //   const expenseTitle = 'Car Insurance';
   //   const expenseAmount = 294.67;
+  const [title, setTitle] = useState(props.title);
+
+  const updateTitle=()=>{
+    setTitle('Updated!');
+  }
 
   // divide the component into two as date can be maintained by passing props
   return (
     <div className="expense-item">
-      {/* <div>
-        <div>{month} - {day} - {year}</div>
-      </div> */}
-      {/* <div>{props.date.toISOString()}</div> */}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={updateTitle}>Update</button>
       </div>
     </div>
   );
