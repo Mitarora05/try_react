97e1cf839fc570cb939b9ccb9c9971708c3ea365
mitarora <mitarora@deloitte.com>
2023-04-14 03:42:38 +0530

    handle error msg

--- src/http-handson/MoviesApp.tsx
+++ src/http-handson/MoviesApp.tsx
@@ -19,11 +19,18 @@ function MoviesApp() {
   //   ];
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/");
+    setError(null);
+    try{
+      const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
+
+    if(!response.ok) {
+      throw new Error('Something went wrong fetching movies')
+    }
 
     const transformedMovies = data.results.map((movieData: any) => {
       return {
@@ -34,6 +41,10 @@ function MoviesApp() {
       };
     });
     setMovies(transformedMovies);
+    
+    }catch(error: any){
+      setError(error.message);
+    }
     setIsLoading(false);
   }
 
@@ -44,7 +55,8 @@ function MoviesApp() {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
+        {!isLoading && error && <p>{error}</p>}
         {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
