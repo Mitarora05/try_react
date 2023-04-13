3819ff985691b3d0a53610afe83f8780d01bbc55
mitarora <mitarora@deloitte.com>
2023-04-14 03:17:29 +0530

    fetch api

--- src/http-handson/movies/Movies.tsx
+++ src/http-handson/movies/Movies.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import classes from './Movie.module.css';
+import classes from './Movies.module.css';
 
 interface MovieProps {
   title: string;
--- src/http-handson/movies/MoviesList.tsx
+++ src/http-handson/movies/MoviesList.tsx
@@ -1,34 +1,31 @@
 import React from 'react';
-
 import Movies from './Movies';
 import classes from './MoviesList.module.css';
-// import MoviesApp from '../MoviesApp';
 
 interface Movie {
-    id: string | number;
-    title: string;
-    releaseDate: string;
-    openingText: string;
-  }
+  id: React.Key;
+  title: string;
+  releaseDate: string;
+  openingText: string;
+}
 
 interface MovieListProps {
-  map(arg0: (movie: { id: React.Key | null | undefined; title: string; releaseDate: string; openingText: string; }) => JSX.Element): React.ReactNode;
   movies: Movie[];
 }
 
-const MovieList: React.FC<MovieListProps> = (movies) => {
-    return (
-      <ul className={classes['movies-list']}>
-        {movies.map((movie: { id: React.Key | null | undefined; title: string; releaseDate: string; openingText: string; }) => (
-          <Movies
-            key={movie.id}
-            title={movie.title}
-            releaseDate={movie.releaseDate}
-            openingText={movie.openingText}
-          />
-        ))}
-      </ul>
-    );
+const MovieList = ({ movies }: MovieListProps): JSX.Element => {
+  return (
+    <ul className={classes['movies-list']}>
+      {movies.map((movie: Movie) => (
+        <Movies
+          key={movie.id}
+          title={movie.title}
+          releaseDate={movie.releaseDate}
+          openingText={movie.openingText}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default MovieList;
